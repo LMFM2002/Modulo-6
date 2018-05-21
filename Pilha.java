64b9bef6904265c207b1e373f553e1d464d2a772
LMFM2002 <32169116+LMFM2002@users.noreply.github.com>
2018-05-21 11:03:53 +0100

    Add files via upload

---
+++ Pilha.java
@@ -0,0 +1,70 @@
+package pilha;
+
+public class Pilha {
+
+	public Object[] pilha;
+    public int posicaoPilha;
+
+    public Pilha() {
+        this.posicaoPilha = -1;
+
+        this.pilha = new Object[1000];
+
+    }
+
+    public boolean pilhaVazia() {
+        //isEmpty
+        if (this.posicaoPilha == -1) {
+            return true;
+        }
+        return false;
+    }
+
+    public int tamanho() {
+   
+        if (this.pilhaVazia()) {
+            return 0;
+        }
+        return this.posicaoPilha + 1;
+    }
+
+    public Object exibeUltimoValor() {
+  
+        if (this.pilhaVazia()) {
+            return null;
+        }
+        return this.pilha[this.posicaoPilha];
+    }
+
+    public Object desempilhar() {
+        //pop
+        if (pilhaVazia()) {
+            return null;
+        }
+        return this.pilha[this.posicaoPilha--];
+    }
+
+    public void empilhar(Object valor) {
+        // push
+        if (this.posicaoPilha < this.pilha.length - 1) {
+            this.pilha[++posicaoPilha] = valor;
+        }
+    }
+
+    public static void main(String args[]) {
+        Pilha p = new Pilha();
+        p.empilhar("Portuguesa ");
+        p.empilhar("Frango com catupiry ");
+        p.empilhar("Calabresa ");
+        p.empilhar("Quatro queijos ");
+        p.empilhar(10);
+                while (p.pilhaVazia() == false) {
+            System.out.println(p.desempilhar());
+        }
+    }
+
+
+
+	}
+
+
